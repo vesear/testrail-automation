80bf67a053fd6bcebc494dbc738103d4ac08c843
Larry2799 <artyomveselko@gmail.com>
2023-06-14 14:02:26 +0200

    feature/TR-10: Fixed comments

--- services/api/projectServices.js
+++ services/api/projectServices.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const ENDPOINTS = {
   ADD_PROJECT: "https://avesear.testrail.io/index.php?/api/v2/add_project",
   GET_PROJECTS: "https://avesear.testrail.io/index.php?/api/v2/get_projects",
-  DELETE_ALL_PROJECTS:
+  DELETE_PROJECT:
     "https://avesear.testrail.io/index.php?/api/v2/delete_project/",
 };
 
@@ -12,11 +12,10 @@ const addProject = async (project, auth) => {
 };
 
 const deleteAllProjects = async (auth) => {
-  console.log({ auth });
   const response = await axios.get(ENDPOINTS.GET_PROJECTS, { auth });
-  const projects = response.data.projects;
+  const projects = response.data?.projects ?? [];
   for (const project of projects) {
-    await axios.post(`${ENDPOINTS.DELETE_ALL_PROJECTS}${project.id}`, null, {
+    await axios.post(`${ENDPOINTS.DELETE_PROJECT}${project.id}`, null, {
       auth,
       headers: {
         "Content-Type": "application/json",
